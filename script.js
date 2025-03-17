8f81d9de4eecc8ec9e69bd30f8991a4e9d830929
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-03-17 12:34:36 -0600

    Add function to play round and a function to play the game for n rounds
    Added a function to play one round of the game and get a winner. Then created a function to play the game for n rounds and update the global score of the game.

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max); 
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
     let humanChoice = prompt("Please choose 'rock', 'paper', or 'scissors':").toLowerCase();
     if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
@@ -21,4 +19,52 @@ function getHumanChoice() {
     }
 }
 
-console.log(getHumanChoice());
+function playRound(roundNumber) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    console.log(`\n✨ Round #${roundNumber} Result ✨\n----------------------------\nYour choice: ${humanSelection}\nComputer choice: ${computerSelection}\n`);
+    return roundWinner = getWinner(humanSelection, computerSelection);
+}
+
+function getWinner(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "tie";
+    } else if (humanChoice === "rock"      && computerChoice === "scissors" ||
+               humanChoice === "paper"     && computerChoice === "rock"     ||
+               humanChoice === "scissors"  && computerChoice === "paper"    ) {
+        return "human";
+    } else {
+        return "computer";
+    }
+}
+
+function playGame(roundsNumber) {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for (i = 0; i < roundsNumber; i++) {
+        let roundWinner = playRound(i+1);
+        if (roundWinner === "human") {
+            console.log("Round Winner: You! 🏆");
+            humanScore += 1;
+        } else if (roundWinner === "computer") {
+            console.log("Round Winner: Computer 😢");
+            computerScore += 1;
+        } else {
+            console.log("Round Winner: It's a tie! 🎉");
+        }
+        console.log(`\n🌎 Current Score 🌎\n-----------------------------\n👤 You: ${humanScore}  |  🤖 Computer: ${computerScore}\n`);
+    }
+
+    console.log(`\n🏁 Final Result 🏁\n-----------------------------\n`);
+    if (humanScore > computerScore) {
+        console.log(`\n🎉 YOU WIN! 🎉\n-----------------------------\n👤 You: ${humanScore}  |  🤖 Computer: ${computerScore}\n🌟 Congratulations, you're ahead! 🌟\n`);
+    } else if (computerScore > humanScore) {
+        cconsole.log(`\n😱 COMPUTER WINS! 😱\n-----------------------------\n👤 You: ${humanScore}  |  🤖 Computer: ${computerScore}\n💥 Better luck next time! 💥\n`);
+    } else {
+        console.log(`\n⚖️ IT'S A TIE! ⚖️\n-----------------------------\n👤 You: ${humanScore}  |  🤖 Computer: ${computerScore}\n🌀 It's a close match! Try again! 🌀\n`);
+    }
+}
+
+playGame(5);
+
