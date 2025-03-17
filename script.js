54c8cf9bc0ab83af55bf1905fc8655703115f7ac
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-03-17 10:35:24 -0600

    Add function to get the computer choice
    Added a function that returns a random choice (“rock”, “paper” or “scissors”) simulating the computer play.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello, World!");
+
+function getComputerChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    return choices[getRandomInt(3)];
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max); 
+}
+
+console.log(getComputerChoice());
