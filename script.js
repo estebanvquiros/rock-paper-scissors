9c6044d83bc6e03936ff9237616f99976b266dfa
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-07-16 15:21:44 -0600

    Create function to get the computer's choice
    Created a function that performs the computer's
    choice in a random way.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+console.log("Welcome to the Rock Paper Scissors Game!");
+
+const OPTIONS = ["ROCK", "PAPER", "SCISSORS"];
+
+function getComputerChoice() {
+    return OPTIONS[Math.floor(Math.random() * 3)];
+}
+
+console.log(getComputerChoice());
