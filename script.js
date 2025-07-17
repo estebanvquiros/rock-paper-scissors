e08ddac4d1f7765676315b8aa7dd14ad01098a42
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-07-16 20:22:20 -0600

    Add function to play one round.
    Added a function that plays one round of the game,
    updates the global score, and declares the winner.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let computerScore = 0;
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
-    switch(randomChoice){
+    switch (randomChoice) {
         case 0:
             return "ROCK";
         case 1:
@@ -18,4 +18,23 @@ function getComputerChoice() {
 function getHumanChoice() {
     let choice = prompt("Enter your choice:\n ROCK | PAPER | SCISSORS").toUpperCase();
     return choice;
+}
+
+function playRound(computerChoice, humanChoice) {
+    if (
+        computerChoice === "PAPER" && humanChoice === "ROCK" ||
+        computerChoice === "SCISSORS" && humanChoice === "PAPER" ||
+        computerChoice === "ROCK" && humanChoice === "SCISSORS"
+    ) {
+        computerScore += 1;
+        console.log("You lose!");
+        console.log(`${computerChoice} beats ${humanChoice}`);
+    } else if (computerChoice === humanChoice) {
+        console.log("It's a tie!");
+        console.log(`${computerChoice} vs ${humanChoice}`);
+    } else {
+        humanScore += 1;
+        console.log("You win!")
+        console.log(`${humanChoice} beats ${computerChoice}`);
+    }
 }
