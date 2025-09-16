9f6c23e808ad59ca7f162437cb59a5d81434ef64
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-09-15 18:31:10 -0600

    Add function to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Welcome to the Rock, Paper, Scissors Game!");
+
+function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random()*3)];
+}
