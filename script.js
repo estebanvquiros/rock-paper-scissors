195df7ab0e2b2a9e1b6d84838fd25c924d895338
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-09-30 18:05:39 -0600

    Remove unused function and console log
    Removed old function to get the user choice and
    irrelevant console log.

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-console.log("Welcome to the Rock, Paper, Scissors Game!");
 
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random()*3)];
-}
-
-function getHumanChoice() {
-    return prompt("Make your choice:\n\nRock\nPaper\nScissors").toLowerCase();
 }
 
 const userChoiceResult = document.querySelector("#user-choice");
